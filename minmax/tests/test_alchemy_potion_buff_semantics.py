7b6e067856c1f28a7b9025ba1fa12e1bcb17ba3a
Jarakeen <jarakeene@gmail.com>
2026-09-07 02:24:24 -0400

    test: cover armor potion named buff routing

--- minmax/tests/test_alchemy_potion_buff_semantics.py
+++ minmax/tests/test_alchemy_potion_buff_semantics.py
@@ -8,6 +8,8 @@ def test_u50_standard_potion_traits_route_to_named_buffs():
     assert potion_buff_for_trait("Restore Health") == "Major Fortitude"
     assert potion_buff_for_trait("Restore Magicka") == "Major Intellect"
     assert potion_buff_for_trait("Restore Stamina") == "Major Endurance"
+    assert potion_buff_for_trait("Increase Armor") == "Major Resolve"
+    assert potion_buff_for_trait("Increase Spell Resist") == "Major Resolve"
     assert potion_buff_for_trait("Increase Spell Power") == "Major Sorcery"
     assert potion_buff_for_trait("Increase Weapon Power") == "Major Brutality"
     assert potion_buff_for_trait("Spell Critical") == "Major Prophecy"
@@ -15,6 +17,8 @@ def test_u50_standard_potion_traits_route_to_named_buffs():
 
 
 def test_u51_consolidated_traits_route_to_surviving_named_buffs():
+    assert potion_buff_for_trait("Increase Armor", game_update=GameUpdate.U51) == "Major Resolve"
+    assert potion_buff_for_trait("Increase Spell Resist", game_update=GameUpdate.U51) == "Major Resolve"
     assert potion_buff_for_trait("Increase Power", game_update=GameUpdate.U51) == "Major Brutality"
     assert potion_buff_for_trait("Critical", game_update=GameUpdate.U51) == "Major Savagery"
     assert potion_buff_for_trait("Increase Spell Power", game_update=GameUpdate.U51) is None
