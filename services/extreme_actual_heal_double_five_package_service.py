15f6127f7ccd9bdb62eab7fa3031a0eac9d8429f
Jarakeen <jarakeene@gmail.com>
2026-09-08 18:41:54 -0400

    feat: include resource objectives in double-five heal packages

--- services/extreme_actual_heal_double_five_package_service.py
+++ services/extreme_actual_heal_double_five_package_service.py
@@ -36,6 +36,9 @@ class ExtremeActualHealDoubleFivePackageService:
         "critical_healing",
         "spell_damage",
         "weapon_damage",
+        "max_health",
+        "max_magicka",
+        "max_stamina",
     )
 
     def __init__(self, database_path: str | Path) -> None:
