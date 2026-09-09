a8fdba96f41a3d7f13bd7afbc1fb32e543938ab9
Jarakeen <jarakeene@gmail.com>
2026-09-09 15:27:59 -0400

    feat: wire encounter service catalog

--- services/foundation_catalog_descriptors.py
+++ services/foundation_catalog_descriptors.py
@@ -8,6 +8,7 @@ wiring rather than resolving services dynamically from catalog strings.
 """
 
 from services.collection_catalog_descriptors import COLLECTION_SERVICE_DESCRIPTORS
+from services.encounter_catalog_descriptors import ENCOUNTER_SERVICE_DESCRIPTORS
 from services.infrastructure_catalog_descriptors import INFRASTRUCTURE_SERVICE_DESCRIPTORS
 from services.reference_catalog_descriptors import REFERENCE_SERVICE_DESCRIPTORS
 from services.service_catalog import EvidenceClass, ServiceBehavior, ServiceDescriptor
@@ -57,6 +58,7 @@ FOUNDATION_SERVICE_DESCRIPTORS: tuple[ServiceDescriptor, ...] = (
     *REFERENCE_SERVICE_DESCRIPTORS,
     *COLLECTION_SERVICE_DESCRIPTORS,
     *INFRASTRUCTURE_SERVICE_DESCRIPTORS,
+    *ENCOUNTER_SERVICE_DESCRIPTORS,
 )
 
 
