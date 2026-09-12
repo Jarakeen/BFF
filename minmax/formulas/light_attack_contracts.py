87d3ea9b92e2e1f3f0f164556c5781138e896db3
Jarakeen <jarakeene@gmail.com>
2026-09-12 16:10:25 -0400

    docs: catalog bow light attack contract

--- minmax/formulas/light_attack_contracts.py
+++ minmax/formulas/light_attack_contracts.py
@@ -174,4 +174,66 @@ LIGHT_ATTACK_CONTRACTS = {
             },
         },
     },
+
+    "bow": {
+        "formula": "calculate_la_bow",
+        "inputs": {
+            "magicka": {
+                "layer": "core",
+                "application": "base",
+            },
+            "stamina": {
+                "layer": "core",
+                "application": "base",
+            },
+            "la_physical_weapon_damage": {
+                "layer": "derived",
+                "application": "base",
+            },
+            "la_physical_spell_damage": {
+                "layer": "derived",
+                "application": "base",
+            },
+            "skill2_la_damage": {
+                "layer": "modifier",
+                "application": "additive_to_base",
+            },
+            "cp_la_damage": {
+                "layer": "modifier",
+                "application": "multiplier",
+            },
+            "skill_la_damage": {
+                "layer": "modifier",
+                "application": "multiplier",
+            },
+            "set_la_damage": {
+                "layer": "modifier",
+                "application": "multiplier",
+            },
+            "bow_damage_done": {
+                "layer": "damage_done",
+                "application": "multiplier",
+            },
+            "physical_damage_done": {
+                "layer": "damage_done",
+                "application": "multiplier",
+            },
+            "damage_done": {
+                "layer": "damage_done",
+                "application": "multiplier",
+            },
+            "direct_damage_done": {
+                "layer": "damage_done",
+                "application": "multiplier",
+            },
+            "single_target_damage_done": {
+                "layer": "damage_done",
+                "application": "multiplier",
+            },
+            "skill_line_damage_bow": {
+                "layer": "damage_done",
+                "application": "multiplier",
+            },
+        },
+    },
 }
