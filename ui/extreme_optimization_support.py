a4ae42913b2bf0ac87659af5387f42a42758bf0b
Jarakeen <jarakeene@gmail.com>
2026-09-13 15:49:31 -0400

    ui: install proven max health record card

--- ui/extreme_optimization_support.py
+++ ui/extreme_optimization_support.py
@@ -19,6 +19,7 @@ def install() -> None:
     from ui.extreme_critical_profile_support import install as install_extreme_critical_profile_support
     from ui.extreme_class_configuration_support import install as install_extreme_class_configuration_support
     from ui.extreme_blueprint_result_support import install as install_extreme_blueprint_result_support
+    from ui.extreme_max_health_record_support import install as install_extreme_max_health_record_support
     from ui.extreme_max_magicka_record_support import install as install_extreme_max_magicka_record_support
     from ui.extreme_optimization_page import ExtremeOptimizationPage
 
@@ -26,6 +27,7 @@ def install() -> None:
     install_extreme_class_configuration_support()
     install_extreme_blueprint_result_support()
     install_extreme_max_magicka_record_support()
+    install_extreme_max_health_record_support()
     original_build_ui = main_window.MainWindow.build_ui
 
     def build_ui_with_extreme_lab(self) -> None:
