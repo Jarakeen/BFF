34f6fc46d2804a4a4519752dfa375d926f1ed875
Jarakeen <jarakeene@gmail.com>
2026-09-13 21:59:11 -0400

    Install Health Recovery record support

--- ui/extreme_optimization_support.py
+++ ui/extreme_optimization_support.py
@@ -19,6 +19,7 @@ def install() -> None:
     from ui.extreme_critical_profile_support import install as install_extreme_critical_profile_support
     from ui.extreme_class_configuration_support import install as install_extreme_class_configuration_support
     from ui.extreme_blueprint_result_support import install as install_extreme_blueprint_result_support
+    from ui.extreme_health_recovery_record_support import install as install_extreme_health_recovery_record_support
     from ui.extreme_max_health_record_support import install as install_extreme_max_health_record_support
     from ui.extreme_max_magicka_record_support import install as install_extreme_max_magicka_record_support
     from ui.extreme_max_stamina_record_support import install as install_extreme_max_stamina_record_support
@@ -30,6 +31,7 @@ def install() -> None:
     install_extreme_max_magicka_record_support()
     install_extreme_max_health_record_support()
     install_extreme_max_stamina_record_support()
+    install_extreme_health_recovery_record_support()
     original_build_ui = main_window.MainWindow.build_ui
 
     def build_ui_with_extreme_lab(self) -> None:
