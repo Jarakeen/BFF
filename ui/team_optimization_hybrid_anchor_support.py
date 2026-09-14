4a9a28ea048c8dd5437adc294d0012635c0f6a55
Jarakeen <jarakeene@gmail.com>
2026-09-14 02:51:01 -0400

    Install Tank provider scope Rotation transfer

--- ui/team_optimization_hybrid_anchor_support.py
+++ ui/team_optimization_hybrid_anchor_support.py
@@ -178,3 +178,8 @@ def install() -> None:
         install as install_coverage_group_effect_catalog_support,
     )
     install_coverage_group_effect_catalog_support()
+
+    from ui.rotation_tank_provider_scope_transfer_support import (
+        install as install_rotation_tank_provider_scope_transfer_support,
+    )
+    install_rotation_tank_provider_scope_transfer_support()
