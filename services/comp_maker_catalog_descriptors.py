e1eb61992775341c520fe6369c885dabad1fcb93
Jarakeen <jarakeene@gmail.com>
2026-09-13 22:46:35 -0400

    Install tank rotation catalog descriptors

--- services/comp_maker_catalog_descriptors.py
+++ services/comp_maker_catalog_descriptors.py
@@ -28,6 +28,7 @@ from services.rotation_gameplay_policy_catalog_descriptors import (
 from services.rotation_observation_catalog_descriptors import (
     ROTATION_OBSERVATION_SERVICE_DESCRIPTORS,
 )
+from services.rotation_tank_catalog_descriptors import ROTATION_TANK_SERVICE_DESCRIPTORS
 from services.team_prescription_catalog_descriptors import (
     TEAM_PRESCRIPTION_SERVICE_DESCRIPTORS,
 )
@@ -146,6 +147,7 @@ COMP_MAKER_SERVICE_DESCRIPTORS: tuple[ServiceDescriptor, ...] = (
     *ROTATION_DD_PERIODIC_SERVICE_DESCRIPTORS,
     *ROTATION_GAMEPLAY_POLICY_SERVICE_DESCRIPTORS,
     *ROTATION_OBSERVATION_SERVICE_DESCRIPTORS,
+    *ROTATION_TANK_SERVICE_DESCRIPTORS,
     *TEAM_PRESCRIPTION_SERVICE_DESCRIPTORS,
     *TEAM_PROVIDER_SERVICE_DESCRIPTORS,
     *TEAM_WORKFLOW_SERVICE_DESCRIPTORS,
