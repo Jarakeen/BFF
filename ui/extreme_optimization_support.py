5b76e2f1a910c255b7098e65d8c9f5842ae7ee04
Jarakeen <jarakeene@gmail.com>
2026-09-15 15:14:37 -0400

    Keep Extreme Optimization support feature-only

--- ui/extreme_optimization_support.py
+++ ui/extreme_optimization_support.py
@@ -16,26 +16,8 @@ def install() -> None:
         ExtremeCompleteOptimizationService,
     )
     from ui import main_window
-    from ui.extreme_critical_profile_support import install as install_extreme_critical_profile_support
-    from ui.extreme_class_configuration_support import install as install_extreme_class_configuration_support
-    from ui.extreme_blueprint_result_support import install as install_extreme_blueprint_result_support
-    from ui.extreme_health_recovery_record_support import install as install_extreme_health_recovery_record_support
-    from ui.extreme_magicka_recovery_record_support import install as install_extreme_magicka_recovery_record_support
-    from ui.extreme_stamina_recovery_record_support import install as install_extreme_stamina_recovery_record_support
-    from ui.extreme_max_health_record_support import install as install_extreme_max_health_record_support
-    from ui.extreme_max_magicka_record_support import install as install_extreme_max_magicka_record_support
-    from ui.extreme_max_stamina_record_support import install as install_extreme_max_stamina_record_support
     from ui.extreme_optimization_page import ExtremeOptimizationPage
 
-    install_extreme_critical_profile_support()
-    install_extreme_class_configuration_support()
-    install_extreme_blueprint_result_support()
-    install_extreme_max_magicka_record_support()
-    install_extreme_max_health_record_support()
-    install_extreme_max_stamina_record_support()
-    install_extreme_health_recovery_record_support()
-    install_extreme_magicka_recovery_record_support()
-    install_extreme_stamina_recovery_record_support()
     original_build_ui = main_window.MainWindow.build_ui
 
     def build_ui_with_extreme_lab(self) -> None:
