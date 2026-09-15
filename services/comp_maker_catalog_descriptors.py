03821fb07578dbb9d29a0b36d74de0dc35eb1939
Jarakeen <jarakeene@gmail.com>
2026-09-15 01:10:50 -0400

    Register Extreme Weapon Damage record service

--- services/comp_maker_catalog_descriptors.py
+++ services/comp_maker_catalog_descriptors.py
@@ -17,6 +17,9 @@ from services.extreme_health_recovery_catalog_descriptors import (
     EXTREME_HEALTH_RECOVERY_SERVICE_DESCRIPTORS,
 )
 from services.extreme_recovery_catalog_descriptors import EXTREME_RECOVERY_SERVICE_DESCRIPTORS
+from services.extreme_weapon_damage_catalog_descriptors import (
+    EXTREME_WEAPON_DAMAGE_SERVICE_DESCRIPTORS,
+)
 from services.raid_plan_catalog_descriptors import RAID_PLAN_SERVICE_DESCRIPTORS
 from services.rotation_catalog_descriptors import ROTATION_SERVICE_DESCRIPTORS
 from services.rotation_dd_catalog_descriptors import ROTATION_DD_SERVICE_DESCRIPTORS
@@ -150,6 +153,7 @@ COMP_MAKER_SERVICE_DESCRIPTORS: tuple[ServiceDescriptor, ...] = (
     *EXTREME_SERVICE_DESCRIPTORS,
     *EXTREME_HEALTH_RECOVERY_SERVICE_DESCRIPTORS,
     *EXTREME_RECOVERY_SERVICE_DESCRIPTORS,
+    *EXTREME_WEAPON_DAMAGE_SERVICE_DESCRIPTORS,
     *ROTATION_SERVICE_DESCRIPTORS,
     *ROTATION_DD_SERVICE_DESCRIPTORS,
     *ROTATION_DD_PERIODIC_SERVICE_DESCRIPTORS,
