cce41c44d917b9164e768d49ae14a3ce47a6d871
Jarakeen <jarakeene@gmail.com>
2026-09-20 22:23:44 -0400

    Add Finch Collaboration to Team navigation

--- ui/components/foundry_sidebar.py
+++ ui/components/foundry_sidebar.py
@@ -50,6 +50,7 @@ CORE_NAV_SECTIONS = [
         ("Coverage", "console:7"),
         ("Comp Builder", "comp_builder"),
         ("Optimizer Adviser", "console:6"),
+        ("Finch Collaboration", "finch_collaboration"),
     ]},
     {"label": "Build", "children": [
         ("Builds", "console:2"),
